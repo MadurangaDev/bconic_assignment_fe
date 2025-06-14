496b7f9679319fa1b1c492d25e501ba8ce343773
Maduranga Jayasena <shehan@teachmeit.lk>
2025-06-14 06:52:49 +0530

    Revenue Units Changed
    ## Fixes
    - Revenue Units Changed

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -202,7 +202,7 @@ export const AdminDashboard: FC = () => {
               <div className="ml-4">
                 <p className="text-sm font-medium text-gray-600">Revenue</p>
                 <p className="text-2xl font-bold text-gray-900">
-                  ${totalRevenue}
+                  {totalRevenue} LKR
                 </p>
               </div>
             </div>
